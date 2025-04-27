fa3c06cc2ef38d3d837ecb03887321cb3deb4f00
DrBPatch5707 <briar2patch@gmail.com>
2025-04-27 08:34:19 -0500

    version v0.6.5

--- modules/login.nix
+++ modules/login.nix
@@ -6,6 +6,10 @@
         description = "Briar Alexander Johnson";
         extraGroups = [ "networkmanager" "wheel" ];
         hashedPasswordFile = "/etc/passwdConfig";
+        password = null;
+        initialPassword = null;
+        hashedPassword = null;
+        initialHashedPassword = null;
         packages = with pkgs; [
         #  thunderbird
         ];
