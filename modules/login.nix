f94fe7324785c320adf5b6a07d1b96df61257d09
DrBPatch5707 <briar2patch@gmail.com>
2025-04-27 08:39:57 -0500

    version v0.6.7

--- modules/login.nix
+++ modules/login.nix
@@ -5,10 +5,10 @@
         isNormalUser = true;
         description = "Briar Alexander Johnson";
         extraGroups = [ "networkmanager" "wheel" ];
-        password = null;
+        password = "$y$j9T$YoLwIiX7vsFWUQ76xsmH.1$FbR/q17wQSqyWaFyr/OSeeFUbPeVuS/HQFzgl3alhN3";
         initialPassword = "$y$j9T$YoLwIiX7vsFWUQ76xsmH.1$FbR/q17wQSqyWaFyr/OSeeFUbPeVuS/HQFzgl3alhN3";
-        hashedPassword = null;
-        initialHashedPassword = null;
+        hashedPassword = "$y$j9T$YoLwIiX7vsFWUQ76xsmH.1$FbR/q17wQSqyWaFyr/OSeeFUbPeVuS/HQFzgl3alhN3";
+        initialHashedPassword = "$y$j9T$YoLwIiX7vsFWUQ76xsmH.1$FbR/q17wQSqyWaFyr/OSeeFUbPeVuS/HQFzgl3alhN3";
         packages = with pkgs; [
         #  thunderbird
         ];
