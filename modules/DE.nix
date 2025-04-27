ef95c4d420ac1889bd007a804ba54c15db8b9b9b
DrBPatch5707 <briar2patch@gmail.com>
2025-04-27 06:44:38 -0500

    version v0.5.9

--- modules/DE.nix
+++ modules/DE.nix
@@ -1,29 +1,32 @@
-    {config, pkgs, ...}:
-    {
-        # Enable the X11 windowing system.
-        services.xserver.enable = true;
+{config, pkgs, ...}:
+{
+    # Enable the X11 windowing system.
+    services.xserver.enable = true;
 
-        # Enable the GNOME Desktop Environment.
-        services.xserver.displayManager.gdm.enable = true;
-        services.xserver.desktopManager.gnome.enable = true;
-        services.gnome.core-utilities.enable = false;
-        environment.sessionVariables = {
-            "GSETTINGS_OVERRIDE" = ''
-            [org/gnome/settings-daemon/plugins/media-keys/custom-keybindings]
-            custom-keybindings=['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']
+    # Enable the GNOME Desktop Environment.
+    services.xserver.displayManager.gdm.enable = true;
+    services.xserver.desktopManager.gnome.enable = true;
+    services.gnome.core-utilities.enable = false;
+    environment.sessionVariables = {
+        "GSETTINGS_OVERRIDE" = ''
+        [org/gnome/settings-daemon/plugins/media-keys/custom-keybindings]
+        custom-keybindings=['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']
 
-            [org/gnome/settings-daemon/plugins/media-keys/custom-keybinding/custom0]
-            name='Launch Console'
-            command='console'
-            binding='<Super>l'
-            '';
+        [org/gnome/settings-daemon/plugins/media-keys/custom-keybinding/custom0]
+        name='Launch Console'
+        command='console'
+        binding='<Super>l'
+        '';
+    };
+        
+        services.dconf = {
+            enable = true;
+            settings = {
+                "org/gnome/desktop/background" = {
+                    picture-uri = "../resources/WYD-background.jpg";
+                    picture-options = "scaled"; # Or "stretched", "centered", "tiled", etc.
+                };
+            };
+
         };
-         
-         dconf.settings = {
-             "org/gnome/desktop/background" = {
-                picture-uri = "../resources/WYD-background.jpg";
-                picture-options = "scaled"; # Or "stretched", "centered", "tiled", etc.
-             };
-        };
-
-    }
+}
