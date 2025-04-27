92ac0d18a07fbc358dca5250c21da5fea50f1c75
DrBPatch5707 <briar2patch@gmail.com>
2025-04-27 06:45:57 -0500

    version v0.6.0

--- configuration.nix
+++ configuration.nix
@@ -12,8 +12,8 @@
 
 
 config = {
-  services.dconf.enable = true; 
 
+  
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
