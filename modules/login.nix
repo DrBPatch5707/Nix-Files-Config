c8d00b6dcaad0050c135343d7b4b399ab5c69f34
DrBPatch5707 <briar2patch@gmail.com>
2025-04-27 08:41:20 -0500

    version v0.6.8

--- modules/login.nix
+++ modules/login.nix
@@ -5,8 +5,8 @@
         isNormalUser = true;
         description = "Briar Alexander Johnson";
         extraGroups = [ "networkmanager" "wheel" ];
-        password = "$y$j9T$YoLwIiX7vsFWUQ76xsmH.1$FbR/q17wQSqyWaFyr/OSeeFUbPeVuS/HQFzgl3alhN3";
-        initialPassword = "$y$j9T$YoLwIiX7vsFWUQ76xsmH.1$FbR/q17wQSqyWaFyr/OSeeFUbPeVuS/HQFzgl3alhN3";
+        password = "18547";
+        initialPassword = "18547";
         hashedPassword = "$y$j9T$YoLwIiX7vsFWUQ76xsmH.1$FbR/q17wQSqyWaFyr/OSeeFUbPeVuS/HQFzgl3alhN3";
         initialHashedPassword = "$y$j9T$YoLwIiX7vsFWUQ76xsmH.1$FbR/q17wQSqyWaFyr/OSeeFUbPeVuS/HQFzgl3alhN3";
         packages = with pkgs; [
