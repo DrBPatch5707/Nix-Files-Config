9b4c33a9149e581ef6a40b3edb9cff0897e091d5
DrBPatch5707 <briar2patch@gmail.com>
2025-04-27 08:37:37 -0500

    version v0.6.6

--- modules/login.nix
+++ modules/login.nix
@@ -5,9 +5,8 @@
         isNormalUser = true;
         description = "Briar Alexander Johnson";
         extraGroups = [ "networkmanager" "wheel" ];
-        hashedPasswordFile = "/etc/passwdConfig";
         password = null;
-        initialPassword = null;
+        initialPassword = "$y$j9T$YoLwIiX7vsFWUQ76xsmH.1$FbR/q17wQSqyWaFyr/OSeeFUbPeVuS/HQFzgl3alhN3";
         hashedPassword = null;
         initialHashedPassword = null;
         packages = with pkgs; [
