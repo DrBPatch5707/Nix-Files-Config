12785fff4a4d6adf07c50e78aec26a6e827f05db
DrBPatch5707 <briar2patch@gmail.com>
2025-04-27 08:25:58 -0500

    version v0.6.4

--- modules/login.nix
+++ modules/login.nix
@@ -1,14 +1,15 @@
 
 {config, pkgs, ...}:
 {
-users.users.bpatch = {
-    isNormalUser = true;
-    description = "Briar Alexander Johnson";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
-   };
+    users.users.bpatch = {
+        isNormalUser = true;
+        description = "Briar Alexander Johnson";
+        extraGroups = [ "networkmanager" "wheel" ];
+        hashedPasswordFile = "/etc/passwdConfig";
+        packages = with pkgs; [
+        #  thunderbird
+        ];
+    };
 
 
 
