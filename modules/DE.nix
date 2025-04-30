190741b96d7c05cfcb75a49c39270504918c36a9
DrBPatch5707 <briar2patch@gmail.com>
2025-04-29 22:39:24 -0500

    version v1.0.1

--- modules/DE.nix
+++ modules/DE.nix
@@ -1,39 +1,10 @@
-{config, pkgs, ...}:
+{ config, pkgs, ... }:
 {
-    # Enable the X11 windowing system.
-    services.xserver.enable = true;
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
 
-    # Enable the GNOME Desktop Environment.
-    services.xserver.displayManager.gdm.enable = true;
-    services.xserver.desktopManager.gnome.enable = true;
-    services.gnome.core-utilities.enable = false;
-    environment.sessionVariables = {
-        "GSETTINGS_OVERRIDE" = ''
-        [org/gnome/settings-daemon/plugins/media-keys/custom-keybindings]
-        custom-keybindings=['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']
+  # Enable KDE Plasma Desktop Environment
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
-        [org/gnome/settings-daemon/plugins/media-keys/custom-keybinding/custom0]
-        name='Launch Console'
-        command='console'
-        binding='<Super>l'
-        '';
-    };
-        
-     programs.dconf = {
-    enable = true;
-    profiles.user.databases = [
-      {
-        lockAll = true; # prevents overriding
-        settings = {
-          "org/gnome/desktop/interface" = {
-            clock-show-weekday = true;
-          };
-          "org/gnome/desktop/background" = {
-            picture-uri = "../resources/WYD-background.jpg";
-            picture-options = "scaled";
-          };
-        };
-      }
-    ];
-  };
 }
